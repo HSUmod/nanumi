b9ea98a27778cbcae97e79322ab3022bd76808f1
Saehyun Kim <saehyun91@gmail.com>
2015-11-23 14:31:01 +0900

    modify login msg

--- MOD_WAS/src/com/nanumi/controller/CommonController.java
+++ MOD_WAS/src/com/nanumi/controller/CommonController.java
@@ -108,16 +108,16 @@ public class CommonController {
 		try {
 			if (user.getPwd().equals(pwd)) {
 				String userUUID = CommonUtils.generateUUID(user.getUserid()); // 로그인 성공, uuid 발급
-				session.setAttribute("UUID-", userUUID); // session에 uuid 저장
+				session.setAttribute("UUID-" + userid, userUUID); // session에 uuid 저장
 				pw.write("{\"result\": \"Success\", \"value\": \"" + userUUID + "\"}");
-				System.out.println("Login success" + userUUID);
+				System.out.println("Login success: " + userUUID);
 			} else {
 				pw.write("{\"result\": \"Fail\", \"value\": \"1\"}");
-				System.out.println("Login fail 01");
+				System.out.println("Login fail: 01");
 			}
 		} catch (NullPointerException e) {
 			pw.write("{\"result\": \"Fail\", \"value\": \"2\"}");
-			System.out.println("Login fail 02");
+			System.out.println("Login fail: 02");
 		} finally {
 			pw.close();
 		}
@@ -125,7 +125,8 @@ public class CommonController {
 
 	@RequestMapping(value = "/Logout.do", method = RequestMethod.POST)
 	public void logout(@RequestParam("userid") String userid, HttpSession session) {
-		session.removeAttribute("UUID_" + userid);
+		session.removeAttribute("UUID-" + userid);
+		System.out.println("Logout success: " + userid);
 	}
 
 	@RequestMapping(value = "/SearchAddress.do")
