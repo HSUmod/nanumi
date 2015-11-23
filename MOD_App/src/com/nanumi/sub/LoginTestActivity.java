44463ff67a28ba497d7fef13016e66b4270747ca
Saehyun Kim <saehyun91@gmail.com>
2015-11-23 10:17:11 +0900

    modify LoginTest

--- MOD_App/src/com/nanumi/sub/LoginTestActivity.java
+++ MOD_App/src/com/nanumi/sub/LoginTestActivity.java
@@ -1,14 +1,32 @@
 package com.nanumi.sub;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.nanumi.R;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-
 
 /**
  * 1. 로그인 처리
@@ -38,11 +56,60 @@ public class LoginTestActivity extends Activity {
 			public void onClick(View v) {
 				String inputId = etId.getText().toString();
 				String inputPw = etPw.getText().toString();
-				// TODO Auto-generated method stub
-
+				
+				LoginReq loginReq = new LoginReq();
+				loginReq.execute(inputId, inputPw);
 			}
 		});
 
 	}
 
+	class LoginReq extends AsyncTask<String, Void, Boolean> {
+		@Override
+		protected Boolean doInBackground(String... param) {
+			JSONObject json = null;
+			boolean flag = false;
+
+			try {
+				ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("userid", param[0]));
+				params.add(new BasicNameValuePair("pwd", param[1]));
+
+				UrlEncodedFormEntity encodeEntity = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+				String postURL = "http://223.194.141.168/MOD_WAS/Login.do";
+				HttpPost post = new HttpPost(postURL);
+				post.setEntity(encodeEntity);
+
+				HttpClient client = new DefaultHttpClient();
+				HttpResponse resPost = client.execute(post);
+				HttpEntity resEntity = resPost.getEntity();
+
+				try {
+					json = new JSONObject(EntityUtils.toString(resEntity));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+				try {
+					if (json.getString("result").startsWith("LOGIN_ERROR")) {
+						Log.d("Login Fail", json.getString("result"));
+						flag = true;
+					} else {
+						String uuid = json.getString("result");
+						Log.d("Login Success", uuid);
+						flag = false;
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return flag;
+
+		}
+	}
 }
