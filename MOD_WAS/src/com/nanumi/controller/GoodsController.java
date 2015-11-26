b08d420ad5dda2c07eab9ad163c30e2c9eb21aae
Saehyun Kim <saehyun91@gmail.com>
2015-11-26 15:40:56 +0900

    added get image byte

--- MOD_WAS/src/com/nanumi/controller/GoodsController.java
+++ MOD_WAS/src/com/nanumi/controller/GoodsController.java
@@ -1,20 +1,21 @@
 package com.nanumi.controller;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URLEncoder;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -61,10 +62,7 @@ public class GoodsController {
 			json.append("\"hashtag\": \"" + item.getHashtag() + "\",");
 			json.append("\"selectionWay\": \"" + item.getSelectionWay() + "\",");
 			json.append("\"postingTime\": \"" + item.getPostingTime() + "\",");
-			json.append("\"image\": \"" + getGoodsImg(item.getArticleNum(), item.getUserid()) + "\"");
-			log.info("===========");
-			log.info(getGoodsImg(item.getArticleNum(), item.getUserid()));
-			log.info("===========");
+			json.append("\"image\": \"" + getImgByte(item.getArticleNum(), item.getUserid()) + "\",");
 			json.append("},");
 		}
 		json.delete(json.length() - 1, json.length()); // last comma delete
@@ -76,17 +74,30 @@ public class GoodsController {
 		pw.close();
 	}
 
-	public ResponseEntity<byte[]> getGoodsImg(String articleNum, String userid) throws Exception {
+	@RequestMapping(value = "/ReadGoodsImg.do", method = RequestMethod.POST)
+	public void getGoodsImg(String articleNum, String userid, HttpServletResponse res) throws Exception {
 		FileDTO file = service.selectFileInfo(articleNum);
+		byte fileByte[] = FileUtils.readFileToByteArray(new File("C:\\dev\\file\\" + userid + "\\" + file.getStored_file_name()));
 
-		Path path = Paths.get("C:\\dev\\file\\" + userid + "\\" + file.getStored_file_name());
-		byte[] data = Files.readAllBytes(path);
+		res.setContentType("application/octet-stream");
+		res.setContentLength(fileByte.length);
+		res.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(file.getOriginal_file_name(), "UTF-8") + "\";");
+		res.setHeader("Content-Transfer-Encoding", "binary");
+		res.getOutputStream().write(fileByte);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.IMAGE_PNG);
-		headers.setContentLength(data.length);
+		res.getOutputStream().flush();
+		res.getOutputStream().close();
+	}
 
-		return new ResponseEntity<byte[]>(data, headers, HttpStatus.CREATED);
+	private byte[] getImgByte(String articleNum, String userid) throws IOException {
+		FileDTO file = service.selectFileInfo(articleNum);
+		File imgPath = new File("C:\\dev\\file\\" + userid + "\\" + file.getStored_file_name());
+		BufferedImage bufferedImage = ImageIO.read(imgPath);
+
+		WritableRaster raster = bufferedImage.getRaster();
+		DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
+
+		return (data.getData());
 	}
 
 	@RequestMapping(value = "/ReadTest.do", method = RequestMethod.POST)
